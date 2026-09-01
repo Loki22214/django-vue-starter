d7c1f2e56db818bae8007ec603e73fc68c7a12fe
Lovro Mišura <misuralovro@gmail.com>
2026-09-02 00:50:59 +0200

    fix(docker): production containers now work properly

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Media files
 MEDIA_URL = "/media/"
